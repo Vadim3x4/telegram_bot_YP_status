17ae31069bb46cc1a35e1944aa67b80d978eb9ae
Vadim3x4 <kurnuli2010@yandex.ru>
2020-09-09 11:59:52 +0400

    homework_v1

--- homework.py
+++ homework.py
@@ -2,7 +2,12 @@ import os
 import requests
 import telegram
 import time
+import logging
 from dotenv import load_dotenv
+
+
+logging.basicConfig(filename='home_work.log', filemode='w', 
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 load_dotenv()
 
@@ -10,41 +15,55 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL_API = os.getenv('URL_API')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    if homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
+    elif homework.get('status') == 'approved':
+        verdict = ('Ревьюеру всё понравилось, можно '
+                   'приступать к следующему уроку.')
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        logging.error('Неверный данные API')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
-    return homework_statuses.json()
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    params = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(
+            URL_API, headers=headers, params=params)
+        if homework_statuses.json().get('homeworks') is None:
+            logging.error("Неверные данные API")
+            print(homework_statuses.json())
+        return homework_statuses.json()
+    except (requests.exceptions.RequestException, ValueError):
+        logging.exception("Неверный ответ сервера")
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+                send_message(parse_homework_status(
+                                        new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get(
+                                        'current_date')
+            time.sleep(300)
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+            logging.exception(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
             continue
 
