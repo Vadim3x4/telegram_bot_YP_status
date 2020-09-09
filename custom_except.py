c558bd479c5565f5130d845a2c121af5591c36be
Vadim3x4 <kurnuli2010@yandex.ru>
2020-09-10 02:18:21 +0400

    v3

---
+++ custom_except.py
@@ -0,0 +1,15 @@
+class SomeBaseProjectException(Exception):
+	pass
+
+class ConnectionException(SomeBaseProjectException):
+	pass
+
+
+class FooException(SomeBaseProjectException):
+	pass
+
+
+class BarException(SomeBaseProjectException):
+	pass
+
+
--- homework.py
+++ homework.py
@@ -4,20 +4,18 @@ import telegram
 import time
 import logging
 from dotenv import load_dotenv
+from custom_except import ConnectionException
 
 logging.basicConfig(filename='app.log', filemode='a',
                     format='%(name)s - %(levelname)s - %(message)s - %(asctime)s')
 
-
-class Connection_error(Exception):
-    pass
 
 
 load_dotenv()
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-URL_API = 'https://praktikum.yandex.ru/api/user_api/homework_statuses1/'
+URL_API = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
@@ -28,22 +26,26 @@ def parse_homework_status(homework):
     elif homework.get('status') == 'approved':
         verdict = ('Ревьюеру всё понравилось, можно '
                    'приступать к следующему уроку.')
+    else:
+        logging.error('Возникла ошибка с получением данных')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
+    if current_timestamp is None:
+        logging.error('Возникла ошибка с форматом даты')
+        return {}
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     params = {'from_date': current_timestamp}
     try:
         homework_statuses = requests.get(
             URL_API, headers=headers, params=params)
         if homework_statuses.status_code != 200:
-            raise Connection_error()
+            raise ConnectionException()
         return homework_statuses.json()
-    except Connection_error:
+    except (requests.exceptions.RequestException, CE):
         logging.error('Ошибка соединения, проверьте URL_API и PRACTICUM_TOKEN')
-        exit()
-
+        
 
 def send_message(message):
     return bot.send_message(chat_id=CHAT_ID, text=message)
@@ -52,14 +54,14 @@ def send_message(message):
 def main():
     current_timestamp = int(time.time())
 
+
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(
+                    send_message(parse_homework_status(
                     new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get(
-                'current_date')
+            current_timestamp = new_homework.get('current_date')
             time.sleep(300)
 
         except Exception as e:
@@ -68,9 +70,10 @@ def main():
             continue
 
 
-try:
-    if __name__ == '__main__':
+
+if __name__ == '__main__':
+    try:
         main()
-except KeyboardInterrupt:
-    print('Quit')
-    exit()
+    except KeyboardInterrupt:
+        print('Quit')
+
